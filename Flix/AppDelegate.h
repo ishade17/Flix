203820b5026e5f1efe9cfc4ccac0d48ed4318c13
Isaac Schaider <ishade17@fb.com>
2020-06-24 11:36:51 -0700

    Initial Commit

---
+++ Flix/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Flix
+//
+//  Created by Isaac Schaider on 6/24/20.
+//  Copyright © 2020 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
